af1fc28df5d96bc0bbb21652d84c0c51b6cd2d0f
Daniel Kulichkov <danchick03@gmail.com>
2024-11-01 13:04:06 +0300

    feat(backup): add backup directory behaviot

--- src/commands/backup_command.rs
+++ src/commands/backup_command.rs
@@ -20,31 +20,47 @@ impl CommandExecutor for BackupCommand {
                 let path = Path::new(path_str);
                 fs::create_dir_all(path).expect("failed"); // TODO
                 path.to_str().unwrap()
-            },
+            }
             None => "",
-        };        
+        };
 
-        if Path::new(&self.path).is_dir() {
+        let path = Path::new(&self.path);
+        if path.is_dir() {
+            let dir_name = path.file_name().unwrap();
+            let dir_name = dir_name.to_str().unwrap();
+            let today_str = utils::generate_date_string_today();
+            let dir_name = format!("{}{}.bak.d", today_str, dir_name);
+            let out_dir = Path::new(out_dir).join(dir_name);
+            let out_dir = out_dir.to_str().unwrap();
+            fs::create_dir_all(out_dir).unwrap();
 
+            for entry in fs::read_dir(path).unwrap() {
+                let entry = entry.unwrap();
+                let entry_path = entry.path();
+                let entry_path = entry_path.to_str().unwrap();
+                backup_file(entry_path, out_dir, false).unwrap();
+            }
         } else {
-            backup_file(&self.path, out_dir).unwrap();
+            backup_file(&self.path, out_dir, true).unwrap();
         }
     }
 }
 
-fn backup_file(original: &str, out_dir: &str) -> Result<(), ()> {
-    let filename = match utils::get_filename(original) {
+fn backup_file(original: &str, out_dir: &str, add_date: bool) -> Result<(), ()> {
+    let mut filename = match utils::get_filename(original) {
         Ok(name) => name,
         Err(_) => return Err(()),
     };
 
-    let today_str = utils::generate_date_string_today();
+    if add_date {
+        let today_str = utils::generate_date_string_today();
+        filename = format!("{}{}.bak", today_str, filename);
+    }
 
-    let filename = format!("{}{}.bak", today_str, filename);
     let out_path = Path::new(out_dir).join(filename);
 
     match fs::copy(original, out_path) {
-        Ok(_) => todo!(),
+        Ok(_) => (),
         Err(e) => eprintln!("{}", e),
     };
     Ok(())
