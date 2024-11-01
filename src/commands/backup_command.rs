0994a2681d287daab05c28b9c67bb0b7045bf61e
Daniel Kulichkov <danchick03@gmail.com>
2024-11-01 12:49:28 +0300

    ref(backup): extract backup function

--- src/commands/backup_command.rs
+++ src/commands/backup_command.rs
@@ -15,27 +15,37 @@ pub struct BackupCommand {
 
 impl CommandExecutor for BackupCommand {
     fn execute(&self) {
-        let filename = match utils::get_filename(&self.path) {
-            Ok(name) => name,
-            Err(_) => todo!(),
-        };
-
-        let today_str = utils::generate_date_string_today();
-
-        let out_path = match &self.out {
-            Some(out) => {
-                let path = Path::new(out);
+        let out_dir = match &self.out {
+            Some(path_str) => {
+                let path = Path::new(path_str);
                 fs::create_dir_all(path).expect("failed"); // TODO
-                let filename = format!("{}{}.bak", today_str, &filename);
-                let path = path.join(filename);
-                path.to_string_lossy().to_string()
+                path.to_str().unwrap()
             },
-            None => format!("{}{}.bak", today_str, &filename),
-        };
+            None => "",
+        };        
 
-        match fs::copy(&self.path, out_path) {
-            Ok(_) => todo!(),
-            Err(e) => eprintln!("{}", e),
-        };
+        if Path::new(&self.path).is_dir() {
+
+        } else {
+            backup_file(&self.path, out_dir).unwrap();
+        }
     }
+}
+
+fn backup_file(original: &str, out_dir: &str) -> Result<(), ()> {
+    let filename = match utils::get_filename(original) {
+        Ok(name) => name,
+        Err(_) => return Err(()),
+    };
+
+    let today_str = utils::generate_date_string_today();
+
+    let filename = format!("{}{}.bak", today_str, filename);
+    let out_path = Path::new(out_dir).join(filename);
+
+    match fs::copy(original, out_path) {
+        Ok(_) => todo!(),
+        Err(e) => eprintln!("{}", e),
+    };
+    Ok(())
 }
