d5d2491609a2adf574c829561df10ff84738260d
Daniel Kulichkov <danchick03@gmail.com>
2024-11-02 15:16:00 +0300

    fix: typo in display

--- src/utils/errors/mod.rs
+++ src/utils/errors/mod.rs
@@ -7,7 +7,7 @@ pub enum Error {
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Error::IoError(error) => write!(f, "IU Error: {}", error),
+            Error::IoError(error) => write!(f, "IO Error: {}", error),
             Error::Other(error) => write!(f, "Error: {}", error),
         }
     }
